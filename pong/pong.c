c55f917fd22b69e3cb7e5cd50abe5d602858f0d8
Max Rose <xorsema@gmail.com>
2014-04-05 16:34:54 -0700

    More network code, still not working yet

--- pong/pong.c
+++ pong/pong.c
@@ -7,7 +7,7 @@
 #include <stdlib.h>
 #include <errno.h>
 
-#define MAXPACKETSIZE 512
+#define MAXPACKETSIZE 0xFFFF
 #define PORTNUM 1200
 
 #define dist_form( x, y ) ( sqrt( ( x * x ) + ( y * y ) ) )
@@ -30,6 +30,7 @@ struct ball
 
 struct gamestate
 {
+	uint32_t time;
 	struct player players[2];
 	struct ball ball;
 };
@@ -89,9 +90,13 @@ struct update_packet
 #pragma pack(pop)
 
 int init();
+int net_init();
+void net_wait_for_game( struct net *pnet );
 void quit();
 void input( SDL_Event );
-void loop();
+void local_loop();
+void client_loop();
+void server_loop();
 void init_gamestate( struct gamestate *g );
 void render_gamestate( struct gamestate *g );
 void white_rect( SDL_Rect * );
@@ -108,8 +113,8 @@ int net_bind( struct net * );
 int net_recv( struct net *pnet, void *outbuf, int buflen, int *outlen, IPaddress *ip );
 int net_send( struct net *pnet, void *inbuf, int inlen, IPaddress to );
 int net_simple_packet( struct net *pnet, struct simple_packet* packet, IPaddress to );
-int net_thread( void * );
-int net_send_update( struct net *pnet, IPaddress to );
+/*int net_thread( void * );*/
+int net_send_update( struct net *pnet, IPaddress to, struct gamestate *gs );
 
 const char *WINDOW_TITLE = "Pong";
 const int WIN_WIDTH = 640;
@@ -172,6 +177,95 @@ int init()
 	init_gamestate( &local_state );
 
 	return ( ( window != NULL ) && ( renderer != NULL ) );
+}
+
+int net_init()
+{
+	if( SDLNet_Init() < 0 )
+	{
+		printf( "Couldn't start SDLNet!\n" );
+		return 0;
+	}
+
+	if( !net_bind( &net ) )
+	{
+		printf( "Could not bind to port!\n" );
+		return 0;
+	}
+}
+
+/* Should be called after a socket has been created */
+void net_wait_for_game( struct net *pnet )
+{
+	uint8_t buf[MAXPACKETSIZE];
+	int buflen = MAXPACKETSIZE;
+	int recvbytes;
+	IPaddress ip;
+	struct simple_packet *sp;
+	struct simple_packet syn_packet;
+	struct simple_packet ack_packet;
+	struct simple_packet synack_packet;
+
+	syn_packet.type = PACKET_SYN;
+	ack_packet.type = PACKET_ACK;
+	synack_packet.type = PACKET_SYNACK;
+
+	if( pnet->type == NET_JOIN )
+	{
+		net_simple_packet( pnet, &syn_packet, pnet->addr );
+		pnet->state = NET_STATE_WAIT_ACK;
+	}
+	else if( pnet->type == NET_HOST )
+	{
+		pnet->state = NET_STATE_WAIT_SYN;
+	}
+	else
+	{
+		return;
+	}
+
+
+	while( pnet->state != NET_STATE_GAME )
+	{
+		if( !net_recv( pnet, (void*)buf, buflen, &recvbytes, &ip ) )
+		{
+			continue;
+		}
+
+		sp = (struct simple_packet*)buf;
+
+		switch( pnet->state )
+		{
+		case NET_STATE_WAIT_SYN:
+			if( sp->type == PACKET_SYN )
+			{
+				printf( "SYN received, sending ACK\n" );
+				pnet->addr.host = ip.host;
+				net_simple_packet( pnet, &ack_packet, pnet->addr );
+				pnet->state = NET_STATE_WAIT_SYNACK;
+			}
+			break;
+		case NET_STATE_WAIT_ACK:
+			if( sp->type == PACKET_ACK )
+			{
+				printf( "ACK received, sending SYNACK\n" );
+				net_simple_packet( pnet, &synack_packet, pnet->addr );
+				pnet->state = NET_STATE_GAME;
+			}
+			break;
+		case NET_STATE_WAIT_SYNACK:
+			if( sp->type == PACKET_SYNACK )
+			{
+				printf( "Got SYNACK\n" );
+				pnet->state = NET_STATE_GAME;
+			}
+			break;
+		default:
+			break;
+		}
+
+		memset( buf, 0, MAXPACKETSIZE );
+	}
 }
 
 void quit()
@@ -246,14 +340,19 @@ void input( SDL_Event event )
 	}
 }
 
-void loop()
+void client_loop()
 {
+	uint8_t buf[MAXPACKETSIZE];
+	int buflen = MAXPACKETSIZE;
+	int recvbytes;
+	IPaddress ip;
+	struct update_packet *up;
 	struct cmd tc;
 	SDL_Event event;
 	Uint32 ticks = SDL_GetTicks();
 	start_time = ticks;
 
-	local_cmd_buf = init_cmd_buf( 512 );
+	local_cmd_buf = init_cmd_buf( 0xFFF );
 
 	while( running )
 	{
@@ -262,37 +361,44 @@ void loop()
 			input( event );
 		}
 
-		if( net.type == NET_LOCAL || net.type == NET_HOST )
+		if( net_recv( &net, (void*)buf, buflen, &recvbytes, &ip ) && ((struct simple_packet*)buf)->type == PACKET_UPDATE )
 		{
-	  		if( input_status[0] )
-			{
-				player_move_cmd( &tc, CMD_PLAYER1_MOVE, -PADDLE_SPEED * ( delta / 1000.f ) );
-				add_to_cmd_buf( local_cmd_buf, tc );
-			}
-			
-			if( input_status[1] )
-			{
-				player_move_cmd( &tc, CMD_PLAYER1_MOVE, PADDLE_SPEED * ( delta / 1000.f ) );
-				add_to_cmd_buf( local_cmd_buf, tc );
-			}
+			up = (struct update_packet*)buf;
+			local_state = up->state;
 		}
 
-		if( net.type == NET_LOCAL || net.type == NET_JOIN )
-		{	
-			if( input_status[3] )
-			{
-				player_move_cmd( &tc, CMD_PLAYER2_MOVE, -PADDLE_SPEED * ( delta / 1000.f ) );
-				add_to_cmd_buf( local_cmd_buf, tc );
-			}
-			
-			if( input_status[2] )
-			{
-				player_move_cmd( &tc, CMD_PLAYER2_MOVE, PADDLE_SPEED * ( delta / 1000.f ) );
-				add_to_cmd_buf( local_cmd_buf, tc );
-			}
+	  	/*if( input_status[0] )
+		{
+			player_move_cmd( &tc, CMD_PLAYER1_MOVE, -PADDLE_SPEED * ( delta / 1000.f ) );
+			add_to_cmd_buf( local_cmd_buf, tc );
 		}
+		
+		if( input_status[1] )
+		{
+			player_move_cmd( &tc, CMD_PLAYER1_MOVE, PADDLE_SPEED * ( delta / 1000.f ) );
+			add_to_cmd_buf( local_cmd_buf, tc );
+		}*/
+			
+		if( input_status[3] )
+		{
+			player_move_cmd( &tc, CMD_PLAYER2_MOVE, -PADDLE_SPEED * ( delta / 1000.f ) );
+			add_to_cmd_buf( local_cmd_buf, tc );
+		}
+		
+		if( input_status[2] )
+		{
+			player_move_cmd( &tc, CMD_PLAYER2_MOVE, PADDLE_SPEED * ( delta / 1000.f ) );
+			add_to_cmd_buf( local_cmd_buf, tc );
+		}
+		
+		net_send_cmd_buf( &net, net.addr, local_cmd_buf );
 
-		advance_gamestate( current_time, delta, 10, &local_state, local_cmd_buf );
+		if( local_state.time < current_time )
+		{
+			advance_gamestate( local_state.time, current_time - local_state.time, 10, &local_state, local_cmd_buf );
+		}
+		else
+			advance_gamestate( local_state.time, delta, 10, &local_state, local_cmd_buf );
 
 		clear_cmd_buf( local_cmd_buf );
 
@@ -304,6 +410,145 @@ void loop()
 
 		delta = SDL_GetTicks() - ticks;
 		current_time = SDL_GetTicks() - start_time;
+		local_state.time = current_time;
+		ticks = SDL_GetTicks();
+	}
+}
+
+void server_loop()
+{
+	uint8_t buf[MAXPACKETSIZE];
+	int buflen = MAXPACKETSIZE;
+	int recvbytes;
+	IPaddress ip;
+	struct cmd_packet *cp;
+	struct cmd tc;
+	SDL_Event event;
+	int i;
+	Uint32 ticks = SDL_GetTicks();
+	start_time = ticks;
+
+	local_cmd_buf = init_cmd_buf( 0xFFF );
+
+	while( running )
+	{
+		while( SDL_PollEvent( &event ) )
+		{
+			input( event );
+		}
+
+		if( net_recv( &net, (void*)buf, buflen, &recvbytes, &ip ) && ((struct simple_packet*)buf)->type == PACKET_CMD )
+		{
+			cp = (struct cmd_packet*)buf;
+			for( i = 0; i < cp->buf.len; i++ )
+			{
+				add_to_cmd_buf( local_cmd_buf, cp->buf.cmds[i] );
+			}
+		}
+
+	  	if( input_status[0] )
+		{
+			player_move_cmd( &tc, CMD_PLAYER1_MOVE, -PADDLE_SPEED * ( delta / 1000.f ) );
+			add_to_cmd_buf( local_cmd_buf, tc );
+		}
+		
+		if( input_status[1] )
+		{
+			player_move_cmd( &tc, CMD_PLAYER1_MOVE, PADDLE_SPEED * ( delta / 1000.f ) );
+			add_to_cmd_buf( local_cmd_buf, tc );
+		}
+			
+		/*if( input_status[3] )
+		{
+			player_move_cmd( &tc, CMD_PLAYER2_MOVE, -PADDLE_SPEED * ( delta / 1000.f ) );
+			add_to_cmd_buf( local_cmd_buf, tc );
+		}
+		
+		if( input_status[2] )
+		{
+			player_move_cmd( &tc, CMD_PLAYER2_MOVE, PADDLE_SPEED * ( delta / 1000.f ) );
+			add_to_cmd_buf( local_cmd_buf, tc );
+		}*/
+		
+		advance_gamestate( local_state.time, delta, 10, &local_state, local_cmd_buf );
+
+		clear_cmd_buf( local_cmd_buf );
+
+		SDL_RenderClear( renderer );
+
+		render_gamestate( &local_state );
+
+		SDL_RenderPresent( renderer );
+
+		net_send_update( &net, net.addr, &local_state );
+
+		delta = SDL_GetTicks() - ticks;
+		current_time = SDL_GetTicks() - start_time;
+		local_state.time = current_time;
+		ticks = SDL_GetTicks();
+	}
+}
+
+void local_loop()
+{
+	SDL_Event event;
+	Uint32 ticks = SDL_GetTicks();
+	start_time = ticks;
+
+	while( running )
+	{
+		while( SDL_PollEvent( &event ) )
+		{
+			input( event );
+		}
+
+		
+	  	if( input_status[0] )
+		{
+			local_state.players[0].offset -= PADDLE_SPEED * ( delta / 1000.f );
+		}
+		
+		if( input_status[1] )
+		{
+			local_state.players[0].offset += PADDLE_SPEED * ( delta / 1000.f );
+		}
+		
+		if( input_status[3] )
+		{
+			local_state.players[1].offset -= PADDLE_SPEED * ( delta / 1000.f );
+		}
+
+		if( input_status[2] )
+		{
+			local_state.players[1].offset += PADDLE_SPEED * ( delta / 1000.f );
+		}
+		
+
+		local_state.players[0].rect[0].y = local_state.players[0].offset;
+		local_state.players[1].rect[0].x = local_state.players[1].offset;
+		local_state.players[0].rect[1].y = local_state.players[0].offset;
+		local_state.players[1].rect[1].x = local_state.players[1].offset;
+
+		
+		local_state.ball.x += local_state.ball.xv * ( delta / 1000.f );
+		local_state.ball.y += local_state.ball.yv * ( delta / 1000.f );
+		
+
+		local_state.ball.rect.x = local_state.ball.x;
+		local_state.ball.rect.y = local_state.ball.y;
+
+		
+		handle_ball( &local_state.ball, &local_state.players[0], &local_state.players[1] );
+
+		SDL_RenderClear( renderer );
+
+		render_gamestate( &local_state );
+
+		SDL_RenderPresent( renderer );
+
+		delta = SDL_GetTicks() - ticks;
+		current_time = SDL_GetTicks() - start_time;
+		local_state.time = current_time;
 		ticks = SDL_GetTicks();
 	}
 }
@@ -320,6 +565,8 @@ void white_rect( SDL_Rect *rect )
 
 void init_gamestate( struct gamestate *g )
 {
+	g->time = 0;
+
 	g->players[0].rect[0].w = PADDLE_WIDTH;
 	g->players[0].rect[0].h = PADDLE_HEIGHT;
 
@@ -421,10 +668,16 @@ void handle_ball( struct ball *pball, struct player *p1p, struct player *p2p )
 
 struct cmd_buf *init_cmd_buf( unsigned size )
 {
-	struct cmd_buf *r = (struct cmd_buf*)malloc( sizeof(struct cmd_buf) );
+	struct cmd_buf *r;
+	
+	printf( "init_cmd_buf called!\n" );
+
+	r = (struct cmd_buf*)malloc( sizeof(struct cmd_buf) );
 	r->cmds = (struct cmd*)malloc( sizeof(struct cmd) * size );
 	r->maxlen = size;
 	r->len = 0;
+
+	return r;
 }
 
 void free_cmd_buf( struct cmd_buf *p )
@@ -580,25 +833,36 @@ int net_simple_packet( struct net *pnet, struct simple_packet *packet, IPaddress
 	return net_send( pnet, (void*)packet, sizeof(struct simple_packet), to );
 }
 
-int net_send_update( struct net *pnet, IPaddress to )
+int net_send_update( struct net *pnet, IPaddress to, struct gamestate *gs )
 {
-	float *fp;
-	uint8_t buf[sizeof(char) + ( sizeof(float) * 6 )];
-	buf[0] = PACKET_UPDATE;
-
-	/*fp = (float*)( buf+1 );
-	fp[0] = player1.offset;
-	fp[1] = player2.offset;
-
-	fp[2] = ball.x;
-	fp[3] = ball.y;
-	fp[4] = ball.xv;
-	fp[5] = ball.yv;
-	*/
-	return net_send( pnet, buf, sizeof(buf), to );
+	struct update_packet up;
+	up.state = *gs;
+	up.type = PACKET_UPDATE;
+	return net_send( pnet, &up, sizeof(struct update_packet), to );
 }
 
-int net_thread( void *ptr )
+int net_send_cmd_buf( struct net *pnet, IPaddress to, struct cmd_buf *in )
+{
+	struct cmd_packet *cp;
+	struct cmd_net_buf *nb;
+	unsigned size;
+	int err;
+
+	size = sizeof( struct cmd_packet ) + ( sizeof( struct cmd ) * ( in->len - 1 ) );
+	cp = (struct cmd_packet *)malloc( size );
+	nb = cmd_to_net( in );
+
+	memcpy( &cp->buf, nb, sizeof( struct cmd_net_buf ) + ( sizeof( struct cmd ) * ( in->len - 1 ) ) );
+
+	cp->type = PACKET_CMD;
+
+	err = net_send( pnet, cp, size, to );
+
+	free( nb );
+	return err;
+}
+
+/*int net_thread( void *ptr )
 {
 	uint8_t buf[MAXPACKETSIZE];
 	int buflen = MAXPACKETSIZE;
@@ -619,7 +883,7 @@ int net_thread( void *ptr )
 
 		sp = (struct simple_packet*)buf;
 
-/*		printf( "Packet received!\n" );  */
+		printf( "Packet received!\n" );  
 
 		switch( pnet->state )
 		{
@@ -670,22 +934,19 @@ int net_thread( void *ptr )
 	}
 
 	return 0;
-}
+}*/
 
-void net_create_thread( struct net *pnet )
+/*void net_create_thread( struct net *pnet )
 {
 	SDL_CreateThread(net_thread, "net", pnet);
-}
+}*/
 
 int main( int argc, char **argv )
 {
-	running = 1;
+	struct simple_packet sp;
 
-	if( SDLNet_Init() < 0 )
-	{
-		printf( "Couldn't start SDLNet!" );
-		return 1;
-	}
+	sp.type = PACKET_SYN;
+	running = 1;
 
 	if( argc > 1 )
 	{
@@ -701,7 +962,6 @@ int main( int argc, char **argv )
 		else if( strcmp( "host", argv[1] ) == 0 )
 		{
 			net.type = NET_HOST;
-			net.state = NET_STATE_WAIT_SYN;
 		}
 		else
 		{
@@ -709,33 +969,18 @@ int main( int argc, char **argv )
 			return 1;
 		}
 
-		if( !net_bind( &net ) )
+		if( !net_init() )
 		{
-			printf( "Could not bind to port, exiting!\n" );
+			printf( "Could not init network, exiting!\n" );
 			return 1;
 		}
 
 		if( net.type == NET_JOIN )
 		{
 			SDLNet_ResolveHost( &net.addr, argv[2], PORTNUM );
-
-			if( !net_simple_packet( &net, PACKET_SYN, net.addr ) )
-			{
-				printf( "simple packet send error: %s", SDLNet_GetError() );
-			}
-			net.state = NET_STATE_WAIT_ACK;
-		}
-		else
-		{
-			net.state = NET_STATE_WAIT_SYN;
 		}
 
-		net_create_thread( &net );
-
-		while( net.state != NET_STATE_GAME )
-		{
-			SDL_Delay( 10 );
-		}
+		net_wait_for_game( &net );
 	}
 	else
 	{
@@ -749,9 +994,20 @@ int main( int argc, char **argv )
 		return 1;
 	}
 	
-	local_state.ball.xv = -BALL_SPEED;
+	switch( net.type )
+	{
+	case NET_LOCAL:
+		local_loop();
+		break;
 
-	loop();
+	case NET_HOST:
+		server_loop();
+		break;
+
+	case NET_JOIN:
+		client_loop();
+		break;
+	}
 
 	quit();
 	return 0;
